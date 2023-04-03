2fb9929a37e4491ae65c9abc0194b0a2eee95535
imankns70 <45065474+imankns70@users.noreply.github.com>
2023-04-03 20:50:22 +0430

    36-radio-button-binding-with-ng-model

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -7,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
   <form class="donut-form" #form="ngForm">
     <label>
       <span>Name</span>
-      <input  type="text" name="name" class="input" ngModel  />
+      <input type="text" name="name" class="input" required ngModel #name="ngModel" />
+      <p>valid:{{name.valid}}</p>
+      <p>invalid:{{name.invalid}}</p>
+      <p>touched:{{name.touched}}</p>
+      <p>untouched:{{name.untouched}}</p>
+      <p>pristine:{{name.pristine}}</p>
+      <p>dirty:{{name.dirty}}</p>
+    </label>
+    <label>
+      <span>Icon</span>
+      <select name="icon" class="input input--select" required ngModel> 
+        <option *ngFor="let icon of icons" [ngValue]="icon | uppercase">{{icon}}</option>
+      </select>
     </label>
     <label>
       <span>Price</span>
-      <input  type="number" name="price" class="input" ngModel  />
+      <input  type="number" name="price" class="input" required ngModel  />
     </label>
     <div class="donut-form-radios">
       <p class="donut-form-radios-label">Promo:</p>
@@ -28,14 +40,30 @@ import { Component, OnInit } from '@angular/core';
         <span>Limited</span>
       </label>
     </div>
+    <label>
+      <span>Description</span>
+      <textarea
+       name="description"
+       class="input input--textarea" required
+       ngModel></textarea>
+    </label>
     <pre>
-      {{form.value | json}}
+      {{form.valid | json}}
     </pre>
   </form>
   `,
 })
 export class DonutFormComponent implements OnInit {
 
+  icons: string[] = [
+    'caramel-swirl',
+    'glazed-fudge',
+    'glazed-fudge',
+    'sour-supreme',
+    'strawberry-glaze',
+    'vanilla-sundae',
+    'zesty-lemon',
+  ]
   ngOnInit(): void {
 
   }
